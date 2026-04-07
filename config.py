b72400e13ef228a5723e5010b3f4568f079db0ca
JAFFER NOVA <jaffernova21@gmail.com>
2026-04-08 02:28:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8797495324:AAENpS3j6hgwPxE-7faf0twfTLhiT8-ZrAc")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8281644724").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://restrictedjaffer:jaffer@321@cluster0.ham0imb.mongodb.net/?appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003527826734"))
