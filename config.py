cec47e89eb96fa7351f922857d0be5638ef3be6c
JAFFER NOVA <jaffernova21@gmail.com>
2026-04-08 02:31:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "35554205"))
+API_HASH = getenv("API_HASH", "7b56a1a17366fb67ba913ff0cbac6e67")
 BOT_TOKEN = getenv("BOT_TOKEN", "8797495324:AAENpS3j6hgwPxE-7faf0twfTLhiT8-ZrAc")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8281644724").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://restrictedjaffer:jaffer@321@cluster0.ham0imb.mongodb.net/?appName=Cluster0")
